d1aee15c31a79f348b3cab30a8fc5e4351c5f217
Cristina Blanco <cristinablanco@MacBook-Pro-de-Cristina-5.local>
2019-03-05 16:51:58 +0100

    done

--- starter-code/routes/passportRouter.js
+++ starter-code/routes/passportRouter.js
@@ -1,17 +1,122 @@
-const express        = require("express");
+const express = require("express");
 const passportRouter = express.Router();
-// Require user model
 
-// Add bcrypt to encrypt passwords
+const User = require("../models/user");
 
-// Add passport 
-
+const bcrypt = require("bcrypt");
+const bcryptSalt = 10;
+const passport = require("passport");
+const session = require("express-session");
+const LocalStrategy = require("passport-local").Strategy;
 
 const ensureLogin = require("connect-ensure-login");
 
 
-passportRouter.get("/private-page", ensureLogin.ensureLoggedIn(), (req, res) => {
-  res.render("passport/private", { user: req.user });
+passport.serializeUser((user, cb) => {
+    cb(null, user._id);
 });
+
+passport.deserializeUser((id, cb) => {
+    User.findById(id, (err, user) => {
+        if (err) { return cb(err); }
+        cb(null, user);
+    });
+});
+
+
+passport.use(new LocalStrategy((username, password, next) => {
+    User.findOne({ username }, (err, user) => {
+        if (err) {
+            return next(err);
+        }
+        if (!user) {
+            return next(null, false, { message: "Incorrect username" });
+        }
+        if (!bcrypt.compareSync(password, user.password)) {
+            return next(null, false, { message: "Incorrect password" });
+        }
+
+        return next(null, user);
+    });
+}));
+
+
+passportRouter.use(session({
+    secret: "our-passport-local-strategy-app",
+    resave: true,
+    saveUninitialized: true
+}));
+
+
+passportRouter.use(passport.initialize());
+passportRouter.use(passport.session());
+
+
+//get-post sign up
+passportRouter.get("/signup", (req, res, next) => {
+    res.render("passport/signup");
+});
+
+
+passportRouter.post("/signup", (req, res, next) => {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    if (username === "" || password === "") {
+        res.render("passport/signup", { message: "Indicate username and password" });
+        return;
+    }
+
+    User.findOne({ username })
+        .then(user => {
+            if (user !== null) {
+                res.render("passport/signup", { message: "The username already exists" });
+                return;
+            }
+
+            const salt = bcrypt.genSaltSync(bcryptSalt);
+            const hashPass = bcrypt.hashSync(password, salt);
+
+            const newUser = new User({
+                username,
+                password: hashPass
+            });
+
+            newUser.save((err) => {
+                if (err) {
+                    res.render("passport/signup", { message: "Something went wrong" });
+                } else {
+                    res.redirect("/");
+                }
+            });
+        })
+        .catch(error => {
+            next(error)
+        })
+});
+
+
+
+
+//get-post login
+passportRouter.get("/login", (req, res, next) => {
+    res.render("passport/login");
+});
+
+passportRouter.post("/login", passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/login",
+    failureFlash: true,
+    passReqToCallback: true
+}))
+
+// Ruta restringida de página privada (sólo con sesión iniciada yay)
+passportRouter.get("/private-page", ensureLogin.ensureLoggedIn(), (req, res) => {
+    res.render("passport/private", { user: req.user });
+});
+
+
+
+
 
 module.exports = passportRouter;
